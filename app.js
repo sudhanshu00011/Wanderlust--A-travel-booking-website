5c4c751784aaaa211f9921c1cd55feddf5c21849
sudhanshu00011 <sudhanshu2004prasad@email.com>
2026-06-20 17:44:35 +0530

    fixed home route

--- app.js
+++ app.js
@@ -92,6 +92,9 @@ app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found !"));
